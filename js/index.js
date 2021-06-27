2a7d7950444f2bad1a3617c946e5a4734357f36a
feeldiac <feeldiac@gmail.com>
2021-06-27 15:48:17 -0500

    Creating a banner for a logo, styling logo and animation

--- js/index.js
+++ js/index.js
@@ -1,3 +1,5 @@
+// Current Time
+
 const clock = document.getElementById('clock');
 
 const getHour = () => {
@@ -11,16 +13,37 @@ const getHour = () => {
 
 setInterval(getHour, 1000);
 
+// Show icon according to current time
+
 const sun = document.querySelector('.sun-icon');
 const moon = document.querySelector('.moon-icon');
 
 const showIcon = () => {
   const hour = new Date().getUTCHours();
-  //   console.log(hour);
-  if (hour > 1 || hour < 12) {
+  console.log(hour);
+  if (hour < 1 || hour > 12) {
+    moon.style.display = 'none';
+    sun.style.display = 'inline-block';
+  } else {
     moon.style.display = 'inline-block';
     sun.style.display = 'none';
   }
 };
 
 showIcon();
+
+// Logo transition
+
+const logo = document.getElementById('logo');
+const words = ['mdn', 'mesita', 'de noche'];
+function getRandomNumber() {
+  return Math.floor(Math.random() * words.length);
+}
+
+const showLogoWords = () => {
+  const randomNumber = getRandomNumber();
+  console.log(randomNumber);
+  logo.innerHTML = words[randomNumber];
+};
+
+setInterval(showLogoWords, 10000);
